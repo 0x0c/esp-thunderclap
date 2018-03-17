c2cc3cb8e5032355e1062c620060ea2b979b9b1f
Akira Matsuda <akira.matsuda@me.com>
2018-03-17 17:22:52 +0900

    Initial commit.

---
+++ include/thunderclap.h
@@ -0,0 +1,35 @@
+//
+//  thunderclap.h
+//  thunderclap
+//
+//  Created by Akira Matsuda on 3/10/16.
+//
+//
+
+#pragma once
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace thunderclap
+{
+template <typename T>
+class thunderclap
+{
+protected:
+	std::map<std::string, std::shared_ptr<std::map<std::string, std::function<void(T x)>>>> table;
+	bool available(std::string name);
+
+public:
+	static thunderclap<T> *shared_instance();
+	void clap(std::string name, T x);
+	std::string watch(std::string name, std::function<void(T x)> f);
+	void dispose(std::string name, std::string hash);
+	void dispose_all(std::string name);
+};
+}
+
+#include "detail/thunderclap.h"
